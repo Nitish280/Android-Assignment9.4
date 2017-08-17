5e775a1d41d82fcda2ad316693062d7955b6237a
Nitish280 <nsrccs@gmail.com>
2017-08-17 06:40:36 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/nitishsingh/android94/MainActivity.java
+++ app/src/main/java/com/example/nitishsingh/android94/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Declaration
     Button btn;
 
     @Override
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
         //here we are adding menu
-        menu.setHeaderTitle("Context menu");
+        menu.setHeaderTitle("Context menu");//setting the title
         menu.add(0,v.getId(),0,"Add");
         menu.add(0,v.getId(),0,"Edit");
         menu.add(0,v.getId(),0,"Delete");
@@ -33,12 +33,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
+        //here we are apllying condition for performing different different condition
         if (item.getTitle() == "Add") {
-            Toast.makeText(this, "Add", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Add", Toast.LENGTH_SHORT).show();//it will show the toast of add
         } else if (item.getTitle() == "Edit") {
-            Toast.makeText(this, "Edit", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Edit", Toast.LENGTH_SHORT).show();//it will show the toast of edit
         } else if (item.getTitle() == "Delete") {
-            Toast.makeText(this, "Delete", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Delete", Toast.LENGTH_SHORT).show();//it will show the toast of delete
         } else {
             return false;
         }
